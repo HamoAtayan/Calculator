e049f8c4045d41599a3d5a517d2cd4300b56d7b5
Hmayak Atayan <hmayak.atayan@gmail.com>
2019-01-11 16:05:23 +0400

    add calculator 2nd case

--- src/com/hatayan/LessonFirst/CalculatorSecond.java
+++ src/com/hatayan/LessonFirst/CalculatorSecond.java
@@ -1,4 +1,26 @@
 package com.hatayan.LessonFirst;
 
 public class CalculatorSecond {
+    public static void main(String[] args) {
+        System.out.println(Sum(1,3));
+        System.out.println(Sub(5,2));
+        System.out.println(Multiple(7,3));
+        System.out.println(Devide(5,2));
+    }
+    public static int Sum(int a, int b){
+        System.out.print(a + " + " + b + " = ");
+        return  a + b;
+    }
+    public static int Sub(int a, int b){
+        System.out.print(a + " - " + b + " = ");
+        return  a - b;
+    }
+    public static int Multiple(int a, int b){
+        System.out.print(a + " * " + b + " = ");
+        return  a * b;
+    }
+    public static double Devide(double a, double b){
+        System.out.print(a + " / " + b + " = ");
+        return  a / b;
+    }
 }
