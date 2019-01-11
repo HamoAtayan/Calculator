864f1c25696d8fa99e4a930d799daba12120ea7a
Hmayak Atayan <hmayak.atayan@gmail.com>
2019-01-11 10:46:27 +0400

    add calculator class

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,25 @@
-public class Main {
+public class Calculator {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Sum(2, 3);
+        Sub(20, 10);
+        Multiple(8, 7);
+        Divide(3, 2);
+    }
+
+    public static void Sum(int FirstNumber, int SecondNumber) {
+        System.out.println(FirstNumber + " + " + SecondNumber + " = " + (FirstNumber + SecondNumber));
+    }
+
+    public static void Sub(int FirstNumber, int SecondNumber) {
+        System.out.println(FirstNumber + " - " + SecondNumber + " = " + (FirstNumber - SecondNumber));
+    }
+
+    public static void Multiple(int FirstNumber, int SecondNumber) {
+        System.out.println(FirstNumber + " * " + SecondNumber + " = " + FirstNumber * SecondNumber);
+    }
+
+    public static void Divide(double FirstNumber, double SecondNumber) {
+        System.out.println(FirstNumber + " / " + SecondNumber + " = " + (FirstNumber / SecondNumber));
     }
 }
