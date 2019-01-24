4f94467f338738a4882d9e894c6221d8408cb377
Hmayak Atayan <hmayak.atayan@gmail.com>
2019-01-24 09:07:31 +0400

    LoopForWithScanner working

--- src/com/hatayan/LessonSecond/LoopForWithScanner.java
+++ src/com/hatayan/LessonSecond/LoopForWithScanner.java
@@ -5,27 +5,31 @@ import java.util.Scanner;
 public class LoopForWithScanner {
    static Scanner scanner = new Scanner(System.in);
 
+   static int FirstNumber = 2;
+   static int SecondNumber = 0;
 
-   static int FirstNumber = scanner.nextInt();
-   static int SecondNumber = scanner.nextInt();
-
-   public static void compareNumbers(){
+   public static int compareNumbers(){
        while (FirstNumber > SecondNumber){
-           System.out.println("1st input cannot be bigger than 2nd input");
-
+           System.out.println("Pleases type First number as First Number cannot be bigger than Second Number");
+           FirstNumber = scanner.nextInt();
+           System.out.println("Please type Second Number");
+           SecondNumber = scanner.nextInt();
        }
+
+       return SecondNumber - FirstNumber;
    }
-   public static void range(){
-       for (int i = FirstNumber; i < (SecondNumber - FirstNumber); i++){
+
+
+   public static void MultiplyOfThree(){
+       for (int i = FirstNumber; i < SecondNumber; i++){
            if (i % 3 == 0){
                System.out.println(i);
            }
-
        }
    }
 
     public static void main(String[] args) {
-        compareNumbers();
-        range();
-    }
-}
+            compareNumbers();
+            MultiplyOfThree();
+            }
+        }
