37bc1b2aa0e4993b821a49fef8f8793cd0563379
Hamo <Hmayak.atayan@gmail.com>
2019-02-02 22:05:47 +0400

    finally duplicateElementsInArray works

--- src/com/hatayan/Lesson4/DuplicateElementsInArray.java
+++ src/com/hatayan/Lesson4/DuplicateElementsInArray.java
@@ -1,4 +1,20 @@
 package com.hatayan.Lesson4;
 
 public class DuplicateElementsInArray {
+    static int[] arr = {1, 2, 5, 2, 8, 5, 12, 12};
+
+    public static void main(String[] args) {
+        returnDuplicate(arr);
+    }
+
+    public static void returnDuplicate(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    System.out.println("Duplicate numbers are :" + array[i]);
+                }
+            }
+        }
+    }
 }
+
